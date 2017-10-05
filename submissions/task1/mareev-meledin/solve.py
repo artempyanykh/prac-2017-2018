a2dd4fa0fb1b79ddcdae7e084bdce859ae6d6729
GlebOlegovich <wowmagic.gm@gmail.com>
2017-10-06 00:32:06 +0300

    Добавление комментариев.

--- submissions/task1/mareev-meledin/solve.py
+++ submissions/task1/mareev-meledin/solve.py
@@ -9,6 +9,7 @@ def nash_equilibrium(in_matrix):
     row_min = np.min(a, 1)
     col_max = np.max(a.T, 1)
 
+    # Проверка игры на равновесие Нэша.
     for i in range(0, n):
         for j in range(0, n):
             if (a[i, j] == row_min[i] and a[i, j] == col_max[j]):
@@ -18,21 +19,26 @@ def nash_equilibrium(in_matrix):
                 q[j] = 1
                 return {'f': a[i, j], 'p': p.tolist(), 'q': q.tolist()}
 
+    # Делаем так, чтобы минимальный элемент матрицы был неотрицательным.
     add = min(0, np.min(a))
     a -= add
 
+    # Формулировка ЗЛП.
     c = np.ones(n)
     a_ub = np.vstack((-a.T, -np.identity(n)))
     b_ub = np.hstack((np.full(n, -1), np.zeros(n)))
 
+    # Поиск спектра стратегии первого игрока и значения игры.
     res = linprog(c, a_ub, b_ub)
     f = 1 / res.fun + add
     p = res.x * res.fun
 
+    # Формулировка ЗЛП.
     c = np.full(n, -1)
     a_ub = np.vstack((a, -np.identity(n)))
     b_ub = np.hstack((np.ones(n), np.zeros(n)))
 
+    # Поиск спектра стратегии второго игрока.
     res = linprog(c, a_ub, b_ub)
     q = res.x * -res.fun
 
