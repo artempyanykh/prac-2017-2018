50871f0a4684f06d80bf9731758713e61f0d4aa0
smvlx <smirnov-sasha2009@yandex.ru>
2017-10-17 22:06:52 +0300

    Update setup.py

--- submissions/task1/blokhina-smirnov/nash/setup.py
+++ submissions/task1/blokhina-smirnov/nash/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='nash',
       version='0.1',
       description='Nash_equilibrium',
-      url='https://github.com/smvlx/nash',
+      url='https://github.com/smvlx/prac-2017-2018/submissions/task1/blokhina-smirnov/nash',
       author='Alexandr Smirnov',
-      author_email='flyingcircus@example.com',
+      author_email='asmv8@ya.ru',
       license='MIT',
       packages=['nash'],
       zip_safe=False)
