f100fe67de6b7593714a2db0fcaa27c86e87c326
smvlx <smirnov-sasha2009@yandex.ru>
2017-10-17 22:09:33 +0300

    Update setup.py

--- submissions/task1/blokhina-smirnov/nash/setup.py
+++ submissions/task1/blokhina-smirnov/nash/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='nash',
       version='0.1',
       description='Nash_equilibrium',
-      url='https://github.com/smvlx/prac-2017-2018/submissions/task1/blokhina-smirnov/nash',
+      url='https://github.com/smvlx/nash',
       author='Alexandr Smirnov',
       author_email='asmv8@ya.ru',
       license='MIT',
