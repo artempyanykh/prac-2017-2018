2105c5a7b9f27a171cef129a2960099e64164b32
ShDaria <31845182+ShDaria@users.noreply.github.com>
2017-11-11 17:12:31 +0300

    Update and rename submissions/task1/shmatenko-agaidarova/tests/Test_nash.py to submissions/task1/shmatenko-agaidarova/Nash_equilibrium/Nash_equilibrium/tests/Test_nash.py

--- submissions/task1/shmatenko-agaidarova/tests/Test_nash.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import numpy as np
-from code.nash import nash_equilibrium
-class Test_Equilibrium(unittest.TestCase):
-    def test_1(self):
-        game_price, strategy1, strategy2 = nash_equilibrium([[0, 0],[0, 0]])
-        self.assertEqual(game_price, 0)
-        np.testing.assert_array_equal([[1, 0], [1, 0]], [strategy1, strategy2])
